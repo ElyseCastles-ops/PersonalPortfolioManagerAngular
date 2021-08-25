1fc56a249326dc98929dd53f5453caf1f112dbd4
Elyse <elyse.castles75@gmail.com>
2021-08-25 12:21:52 -0400

    more fixes yay

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,20 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccountInfoComponent } from './account-info/account-info.component';
 import { CashDetailsComponent } from './cash-details/cash-details.component';
-import { ChildRoutingModule } from './child/child-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { GainersLosersComponent } from './gainers-losers/gainers-losers.component';
 import { InvestmentDetailsComponent } from './investment-details/investment-details.component';
 import { InvestmentInsightsComponent } from './investment-insights/investment-insights.component';
 import { MarketMoversComponent } from './market-movers/market-movers.component';
 
-const routes: Routes = [
+const appRoutes: Routes = [
+  { path: 'investment-details', component: InvestmentDetailsComponent },
+  { path: 'cash-details', component: CashDetailsComponent }
   /*{ path: '', redirectTo: '/heroes', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }*/
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), ChildRoutingModule],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }

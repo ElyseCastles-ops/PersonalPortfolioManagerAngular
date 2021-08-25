1a172aa00479320631831f9644a0593217814484
Elyse <elyse.castles75@gmail.com>
2021-08-25 13:31:19 -0400

    stuff works

--- src/app/investment-insights/investment-insights.component.ts
+++ src/app/investment-insights/investment-insights.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvestmentInsightsComponent implements OnInit {
 
+  toggleVal = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleMM() {
+    this.toggleVal = 0;
+  }
+
+  toggleGL() {
+    this.toggleVal = 1;
   }
 
 }

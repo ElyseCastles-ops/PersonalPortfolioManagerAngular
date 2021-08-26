bfd1838dc12b4f7071e63fe0a75ec138a931f125
kameronscodespace <kameronmontague@pursuit.org>
2021-08-26 15:42:04 -0500

    Filled in the chart

--- src/app/shared/widgets/area/area.component.ts
+++ src/app/shared/widgets/area/area.component.ts
@@ -2,90 +2,83 @@ import { Component, OnInit } from '@angular/core';
 import * as Highcharts from 'highcharts';
 
 @Component({
-  selector: 'app-widget-area',
-  templateUrl: './area.component.html',
-  styleUrls: ['./area.component.css']
+    selector: 'app-widget-area',
+    templateUrl: './area.component.html',
+    styleUrls: ['./area.component.css']
 })
 export class AreaComponent implements OnInit {
 
-  chartOptions = {};
+    chartOptions = {};
 
-  Highcharts = Highcharts;
-  
-  constructor() { }
+    Highcharts = Highcharts;
 
-  ngOnInit(): void {
-    this.chartOptions = {
+    constructor() { }
 
-      title: {
-          text: 'Solar Employment Growth by Sector, 2010-2016'
-      },
-  
-      subtitle: {
-          text: 'Source: thesolarfoundation.com'
-      },
-  
-      yAxis: {
-          title: {
-              text: 'Number of Employees'
-          }
-      },
-  
-      xAxis: {
-          accessibility: {
-              rangeDescription: 'Range: 2010 to 2017'
-          }
-      },
-  
-      legend: {
-          layout: 'vertical',
-          align: 'right',
-          verticalAlign: 'middle'
-      },
-  
-      plotOptions: {
-          series: {
-              label: {
-                  connectorAllowed: false
-              },
-              pointStart: 2010
-          }
-      },
-  
-      series: [{
-          name: 'Installation',
-          data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-      }, {
-          name: 'Manufacturing',
-          data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-      }, {
-          name: 'Sales & Distribution',
-          data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-      }, {
-          name: 'Project Development',
-          data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-      }, {
-          name: 'Other',
-          data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
-      }],
-  
-      responsive: {
-          rules: [{
-              condition: {
-                  maxWidth: 500
-              },
-              chartOptions: {
-                  legend: {
-                      layout: 'horizontal',
-                      align: 'center',
-                      verticalAlign: 'bottom'
-                  }
-              }
-          }]
-      }
-  
-  };
-                
-  }
+    ngOnInit(): void {
+        this.chartOptions = {
+
+            title: {
+                text: 'User Accounts For Year 2021'
+            },
+
+            subtitle: {
+                text: 'Source: thesolarfoundation.com'
+            },
+
+            yAxis: {
+                title: {
+                    text: 'Number of Employees'
+                }
+            },
+
+            xAxis: {
+                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'August']
+
+            },
+
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle'
+                //   },
+
+                //   plotOptions: {
+                //       series: {
+                //           label: {
+                //               connectorAllowed: false
+                //           },
+                //           pointStart: 2021
+                //       }
+            },
+
+            series: [{
+                name: 'Networth',
+                data: [3300, 112126.0291, 129951.3972, 257859.163, 286182.6734, 287547.5756, 301709.1784, 313157.4312]
+            }, {
+                name: 'Investments',
+                data: [0, 102478.9491, 113280.5072, 234164.463, 255464.1634, 249805.2556, 258778.5684, 265038.5312]
+            }, {
+                name: 'Cash',
+                data: [3300, 9647.08, 16670.89, 23694.7, 30718.51, 37742.32, 42930.61, 48118.9]
+            }],
+
+            responsive: {
+                rules: [{
+                    condition: {
+                        maxWidth: 500
+                    },
+                    chartOptions: {
+                        legend: {
+                            layout: 'horizontal',
+                            align: 'center',
+                            verticalAlign: 'bottom'
+                        }
+                    }
+                }]
+            }
+
+        };
+
+    }
 
 }
